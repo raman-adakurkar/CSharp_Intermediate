47d2d3c0eb362f0070f636e553faffd56b587ab1
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-22 14:43:30 +0530

    practiced how to declare class and object and how to use them

---
+++ introToClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace introToClasses
+{
+    public class Person
+    {
+        public void Introduction(string to)
+        {
+            string name = "Raman";
+            Console.WriteLine("Hello {0}, I am {1}. Nice to see you here...", to, name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name : ");
+            string to = Console.ReadLine();
+
+            var person = new Person();
+            Console.WriteLine();
+            person.Introduction(to);
+            Console.WriteLine();
+        }
+    }
+}
