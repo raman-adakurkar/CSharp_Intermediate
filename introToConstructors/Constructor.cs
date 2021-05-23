07db8e4efac9033801a6ee2f2fc52487622bd9d6
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-23 09:02:57 +0530

    learned and tried Constructors

---
+++ introToConstructors/Constructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace introToConstructors
+{
+    public class Constructor
+    {
+        public Constructor()
+        {
+            Console.WriteLine("Default Constructor called");
+        }
+
+        public Constructor(int id, string name)
+        {
+            Console.WriteLine("ID   : " + id);
+            Console.WriteLine("NAME : " + name);
+        }
+    }
+}
---
+++ introToConstructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace introToConstructors
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = 27;
+            var name = "Raman";
+            var c = new Constructor(id, name);
+        }
+    }
+}
