d9fa19c880769aeaa0f62773f1c0c9717c247f39
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-05-28 13:03:54 +0530

    Exercise 2 solved

---
+++ exercise2/Post.cs
@@ -0,0 +1,43 @@
+﻿/*Design a class called Post.This class models a StackOverflow post. It should have properties
+for title, description and the date/time it was created. We should be able to up-vote or down-vote
+a post. We should also be able to see the current vote value. In the main method, create a post,
+up-vote and down-vote it a few times and then display the the current vote value.*/
+
+using System;
+
+namespace exercise2
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _dateTime;
+        private int _voteCount = 0;
+
+        public void CreatePost(string title, string description)
+        {
+            _title = title;
+            _description = description;
+            _dateTime = DateTime.Now;
+
+            Console.WriteLine("Title : " + _title + "\nDescription : " + _description + "\nCreated on : " + _dateTime);
+            
+        }
+
+        public int UpVote()
+        {
+            return _voteCount++;
+        }
+
+        public int DownVote()
+        {
+            return _voteCount--;
+        }
+
+        public int VoteCount()
+        {
+            return _voteCount;
+        }
+
+    }
+}
---
+++ exercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Design a class called Post.This class models a StackOverflow post. It should have properties
+for title, description and the date/time it was created. We should be able to up-vote or down-vote
+a post. We should also be able to see the current vote value. In the main method, create a post,
+up-vote and down-vote it a few times and then display the the current vote value.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Title : ");
+            var title = Console.ReadLine();
+
+            Console.WriteLine("Enter Description : ");
+            var description = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var post = new Post();
+            post.CreatePost(title, description);
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+
+            Console.WriteLine("Current Vote Value : " + post.VoteCount());
+
+        }
+    }
+}
